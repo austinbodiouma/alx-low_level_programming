21a4a729ce22cacc393b88d6330cffe58ba15c36
Austin Bodi <102233259+austinbodiouma@users.noreply.github.com>
2022-07-07 18:33:21 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,1 @@
-#include "main.h"
-/**
- * print_alphabet_x10-prints 10 times the alphabet, in lowercase
- *
- */
-void print_alphabet_x10(void)
-{
-	int k;
-	int i;
 
-	for (k = 0; k < 10; k++)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
-	}
-}
