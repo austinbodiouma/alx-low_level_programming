24c7e3e1b23bea37746796fc3f002d40d22730cf
Austin Bodi <102233259+austinbodiouma@users.noreply.github.com>
2022-07-08 19:38:19 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * print_line -> printing line
- * @n: integer params
+ * print_line - use _ to print lines on terminal
+ * @n: the number of _ to use
  */
 void print_line(int n)
 {
-	int x;
+	int i;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (x = 0; x < n; x++)
-			_putchar(95);
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
